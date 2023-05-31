7f7b8e9865db25f00261de1c4cf94676d8fa7fce
Rafael de Luca <deluca1712@gmail.com>
2023-05-31 15:22:28 -0300

    Criando backend com spring initializer

---
+++ backend/src/test/java/com/rafaeldeluca/painelvendas/PainelVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.painelvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
