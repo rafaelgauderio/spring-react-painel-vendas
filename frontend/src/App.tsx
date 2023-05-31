b867fa6ba9a0d98f48889c3b3842616dc90b3d88
Rafael de Luca <deluca1712@gmail.com>
2023-05-31 17:15:06 -0300

    Adicionando css global

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Painel de Vendas</h1>     
+    <div>
+      <h1 className="text-primary text-center">Painel de Vendas</h1>     
     </div>
   );
 }
