ab4a80ac93afa43e911b9d50ef96c06fe1d887c5
Rafael de Luca <deluca1712@gmail.com>
2023-06-02 16:37:42 -0300

    Controlador Seller findAll Sellers

---
+++ backend/src/main/java/com/rafaeldeluca/painelvendas/resources/SellerResource.java
@@ -0,0 +1,28 @@
+package com.rafaeldeluca.painelvendas.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafaeldeluca.painelvendas.dto.SellerDTO;
+import com.rafaeldeluca.painelvendas.services.SellerService;
+
+@RestController
+@RequestMapping(value="/sellers")
+public class SellerResource {
+	
+	@Autowired
+	private SellerService sellerService;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAllSellers () {
+		
+		List<SellerDTO> listSellerDTO = sellerService.findAll();
+		return ResponseEntity.ok().body(listSellerDTO);
+	}
+
+}
