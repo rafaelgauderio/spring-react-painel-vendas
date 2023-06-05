5981f50cb1ef2f54aab5457d5ae02bddf78bfcc8
Rafael de Luca <deluca1712@gmail.com>
2023-06-05 15:52:50 -0300

    Salvar Seller em memória - otimizando consulta

--- backend/src/main/java/com/rafaeldeluca/painelvendas/services/SaleService.java
+++ backend/src/main/java/com/rafaeldeluca/painelvendas/services/SaleService.java
@@ -3,6 +3,7 @@ package com.rafaeldeluca.painelvendas.services;
 import com.rafaeldeluca.painelvendas.dto.SaleDTO;
 import com.rafaeldeluca.painelvendas.entities.Sale;
 import com.rafaeldeluca.painelvendas.repositories.SaleRepository;
+import com.rafaeldeluca.painelvendas.repositories.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,8 +19,14 @@ public class SaleService {
     @Autowired
     private SaleRepository saleRepository;
 
+    @Autowired
+    private SellerRepository sellerRepository;
+
     @Transactional(readOnly = true)
     public Page<SaleDTO> findALL (Pageable listaPaginada) {
+        // já injetar um busca dos sellers, para que a consutalta não tenha que ficar
+        // voltando várias vezes ao database.
+        sellerRepository.findAll(); // jpa vai deixar salva em memoria os vendedores e não vai voltar ao banco
         Page<Sale> pageSale = saleRepository.findAll(listaPaginada);
         Page<SaleDTO> pageSaleDTO = pageSale.map(sale -> new SaleDTO(sale));
         return  pageSaleDTO;
