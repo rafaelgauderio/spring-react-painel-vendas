a9d3d9664f942f6f1ae7794efe50192fb95fb6a9
Rafael de Luca <deluca1712@gmail.com>
2023-06-06 15:10:00 -0300

    Link entre home e dashboard

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import ImgDeLuca from '../../assets/images/logo_deluca.png';
+import './styles.css';
 
 const NavBar = () => {
 
@@ -7,7 +9,9 @@ const NavBar = () => {
             <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
                 <div className="container">
                     <nav className="my-2 my-md-0 mr-md-3">
-                        <img src={ImgDeLuca} alt="rafaelgauderio" width="100" />
+                        <Link to="/">
+                            <img src={ImgDeLuca} alt="rafaelgauderio" width="100" />
+                        </Link>
                     </nav>
                 </div>
             </div>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,28 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
 
     return (
-        <div>
-            <h1>Página inicial Home</h1>
-        </div>
+        <>
+            <NavBar></NavBar>
+            <div>
+                <div className="container my-5">
+                    <div className="jumbotron">
+                        <h1 className="display-5">Painel de Vendas</h1>
+                        <p className="lead">Análise de Vendas por vendedor</p>
+                        <p className="lead">Total de vendas realizas por valor e taxa de vendas fechadas por visita. </p>
+                        <hr />
+                        <p>Esta API exibe uma dashboard em um frontEnd desenvolvido com React e typescript. E o backend com java e Spring-Boot.</p>
+                        <Link className="btn btn-primary btn-lg" to="/dashboard">
+                            Acessar Painel de Vendas
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <Footer></Footer>
+        </>
     );
 };
 
