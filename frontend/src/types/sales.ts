f5a90c2ef981ea11f738f5ee293e2fc9d5ac5da1
Rafael de Luca <deluca1712@gmail.com>
2023-06-07 14:35:14 -0300

    Tipagem de seller, sales

--- frontend/src/types/sales.ts
+++ frontend/src/types/sales.ts
@@ -1,3 +1,5 @@
+import { Seller } from "./seller";
+
 export type SaleSum = {
     sellerName: string,
     sumSales: number,
@@ -8,3 +10,29 @@ export type ClosedDealsSuccessFee = {
     sumVisitedClients: number,
     sumClosedDeals: number,
 };
+
+export type Sale = {
+    id: number,
+    visitedClients: number,
+    closedDeals: number,
+    quantity: number,
+    date: string,
+    seller: Seller,
+}
+
+export type SalePaged = {
+    content: Sale[];
+    totalElements: number,
+    totalPages: number,
+    last: boolean,
+    size: number,
+    number: number,
+    sort?: {
+        sorted: boolean,
+        unsorted: boolean,
+        empty: boolean
+    },
+    numberOfElements: number,
+    first: boolean,
+    empty: boolean
+}
