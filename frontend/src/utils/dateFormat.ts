1a087dc6f3a32a65eeaa761ff67286a89a9c7fe0
Rafael de Luca <deluca1712@gmail.com>
2023-06-07 14:54:53 -0300

    Biblioteca de formatar data e funcao

---
+++ frontend/src/utils/dateFormat.ts
@@ -0,0 +1,7 @@
+import { format } from 'date-fns';
+
+export const formatLocalDate = (date: string, desingPattern: string) => {
+    const newDate = new Date(date);
+    const newDateOnly =  new Date(newDate.valueOf() + newDate.getTimezoneOffset() * 60 * 1000);
+    return format(newDateOnly, desingPattern);
+};
